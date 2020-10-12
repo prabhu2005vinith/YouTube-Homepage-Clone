278dc99fc30c2361bf641aa5dd03f87bc78962a4
Vinith Prabhu <prabhu.2005.vinith@gmail.com>
2020-10-12 23:04:20 +0530

    Header ready with complete functionality and styling
    Yet to add sidebar and recommended videos session. Sticky functionality of header also added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+            <div className="mainPage">
+                <Sidebar />
+                <RecommendedVideos />
+            </div>
+        </div>
+    );
 }
 
 export default App;
